78ccb67b09a476297f362c1d1636f62ac97e86c4
Artem Priadchenko <yorgan.info@gmail.com>
2023-06-21 00:15:18 +0300

    Завершения части заданий

---
+++ Task_3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+
+
+
+            var prepositions = new[] { "без", "в", "від", "для", "до", "з", "за", "на", "над", "під", "по", "біля", "про", "ради", "через" };
+            //string text = "Виглянув за столу, прийшов до струмка, лежить на столу";
+
+            string text = default;
+
+            FileInfo file = new FileInfo(@"text.txt");
+
+
+            if (!file.Exists)
+            {
+                using (StreamWriter sr = new StreamWriter(file.FullName))
+                {
+                    sr.WriteLine("Виглянув за столу, прийшов до струмка, лежить на столу");
+                }
+            }
+
+            using (StreamReader sr = new StreamReader(file.FullName))
+            {
+                text += sr.ReadToEnd();
+            }
+
+            Console.WriteLine(text);
+
+            foreach (string element in prepositions)
+            {
+                string pattern = $@"\b{element}\b";
+                text = Regex.Replace(text, pattern, "ГАВ", RegexOptions.IgnoreCase);
+            }
+
+            Console.WriteLine(text);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Task_6/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+    internal class Program
+    {
+
+
+        static Dictionary<string, string> user = new Dictionary<string, string>();
+
+
+        static void Main(string[] args)
+        {
+            string login = default;
+            while (true)
+            {
+                try
+                {
+                    string patern = "^[a-z]{1,8}$";
+                    Regex regex = new Regex(patern);
+
+
+
+                    Console.Write("Ведіть логін: ");
+                    login = Console.ReadLine();
+
+                    if (regex.IsMatch(login))
+                    {
+                        break;
+                    }
+
+                    throw new Exception("«Логін», повинен складатися тільки з символів латинського алфавіту " +
+                        "та мати від 1 до 8 символів");
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                    Console.ResetColor();
+                }
+
+            }
+
+            string pasword = default;
+            while (true)
+            {
+                try
+                {
+                    string patern = "^[a-z0-9]{8,16}$";
+                    Regex regex = new Regex(patern);
+
+
+
+                    Console.Write("Ведіть пароль: ");
+                    pasword = Console.ReadLine();
+
+                    if (regex.IsMatch(pasword))
+                    {
+                        break;
+                    }
+
+                    throw new Exception("Невырный формат пароля! Пароль повинен складаться з цифр і символів " +
+                        "та мати від 8 до 16 символів");
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                    Console.ResetColor();
+                }
+
+            }
+
+            user.Add(login,pasword);
+
+            string pasw = Console.ReadLine();
+
+
+
+        }
+    }
+}
