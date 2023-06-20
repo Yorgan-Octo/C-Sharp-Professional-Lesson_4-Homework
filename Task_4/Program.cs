a7b428218c60793ce02cfd67cf4e18cf9f9e910b
Artem Priadchenko <yorgan.info@gmail.com>
2023-06-21 01:28:19 +0300

    {htynf

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Task_4
@@ -24,6 +27,49 @@ namespace Task_4
             //Радуга - 22000.00 грн.
             //Дата покупки  (10.03.2023)
 
+
+
+            using (StreamReader ch = new StreamReader("chek.txt"))
+            {
+                string line;
+                while ((line = ch.ReadLine())!= null)
+                {
+
+                    string pattern = @"Дата покупки";
+
+                    var regex = new Regex(pattern);
+
+                    if (regex.IsMatch(line))
+                    {
+                        //Console.WriteLine("Gjregrf ,skf lfdyyj");
+
+                        Console.WriteLine(Regex.Replace(line,
+                                           @"^Дата покупки: (?<месяц>\d{1,2})\/(?<день>\d{1,2})\/(?<год>\d{2,4})",
+
+                                           (месяц, день, год) => DateTime.Parse(месяц, день, год).ToString("F2", CultureInfo.CurrentCulture));
+
+                        break;
+                    }
+
+
+
+                    Console.WriteLine(Regex.Replace(line,                                 
+                                            @"(?<var1>\d+)",                                                
+                                            var1 => double.Parse(var1.Value).ToString("F2", CultureInfo.CurrentCulture)));
+
+                }
+
+
+
+            }
+
+
+
+
+
+
+
+            Console.ReadKey();
 
 
         }
