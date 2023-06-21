ab8fd188e9fd6491d01e8e6bee0b554acb362f51
Artem Pr <yorgan.info@gmail.com>
2023-06-21 12:01:37 +0300

    Завершенно завдання 3

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -19,58 +19,65 @@ namespace Task_4
             //з певною кількістю найменувань товарів та датою здійснення покупки.
             //Виведіть на екран інформацію з чека у форматі поточної локалі користувача та у форматі локалі en-US.
 
+            FileInfo file = new FileInfo("chek.txt");
 
-            //Футболка - 99.99 грн.
-            //Мобильный телефон - 5000.00 грн.
-            //Книга - 50.00 грн.
-            //Фен - 150.00 грн.
-            //Радуга - 22000.00 грн.
-            //Дата покупки  (10.03.2023)
-
+            if (!file.Exists)
+            {
+                using (StreamWriter wr = new StreamWriter(file.FullName))
+                {
+                    wr.WriteLine("Футболка - 99 грн.");
+                    wr.WriteLine("Мобильный телефон - 500 грн.");
+                    wr.WriteLine("Книга - 50 грн.");
+                    wr.WriteLine("Фен - 150 грн.");
+                    wr.WriteLine("Радуга - 2200 грн.");
+                }
+            }
 
 
             using (StreamReader ch = new StreamReader("chek.txt"))
             {
                 string line;
-                while ((line = ch.ReadLine())!= null)
+                while ((line = ch.ReadLine()) != null)
+                {
+
+                    string pattern = @"Дата покупки";
+
+                    var regex = new Regex(pattern);
+ 
+                    Console.WriteLine(Regex.Replace(line, @"(?<var1>\d+)",var1 => double.Parse(var1.Value).ToString("F2", CultureInfo.CurrentCulture)));
+
+                    Console.WriteLine(new String('-', 50));
+                }
+
+                DateTime data = DateTime.Now;
+                Console.WriteLine($"Дата покупки: " + data.ToString("D", CultureInfo.CurrentCulture));
+
+            }
+
+            Console.WriteLine(new String('=', 50));
+
+            var us = new CultureInfo("en-US");
+            using (StreamReader ch = new StreamReader("chek.txt"))
+            {
+                string line;
+                while ((line = ch.ReadLine()) != null)
                 {
 
                     string pattern = @"Дата покупки";
 
                     var regex = new Regex(pattern);
 
-                    if (regex.IsMatch(line))
-                    {
-                        //Console.WriteLine("Gjregrf ,skf lfdyyj");
-
-                        Console.WriteLine(Regex.Replace(line,
-                                           @"^Дата покупки: (?<месяц>\d{1,2})\/(?<день>\d{1,2})\/(?<год>\d{2,4})",
-
-                                           (месяц, день, год) => DateTime.Parse(месяц, день, год).ToString("F2", CultureInfo.CurrentCulture));
-
-                        break;
-                    }
-
-
-
-                    Console.WriteLine(Regex.Replace(line,                                 
-                                            @"(?<var1>\d+)",                                                
-                                            var1 => double.Parse(var1.Value).ToString("F2", CultureInfo.CurrentCulture)));
-
+                    Console.WriteLine(Regex.Replace(line, @"(?<var1>\d+)", var1 => double.Parse(var1.Value).ToString("F2", us)));
+                    Console.WriteLine(new String('-', 50));
                 }
 
-
+                DateTime data = DateTime.Now;
+                Console.WriteLine($"Дата покупки: " + data.ToString("D", us));
 
             }
 
 
-
-
-
-
-
             Console.ReadKey();
-
 
         }
     }
